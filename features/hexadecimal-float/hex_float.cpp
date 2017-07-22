5de97e581715c0ccd1da90aff7a0150f8df08a51
lostjared <jaredbruni@gmail.com>
2017-07-22 14:41:52 -0700

    hexadecimal floating point (using exponent)  example

---
+++ features/hexadecimal-float/hex_float.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main() {
+	double value1 = 0xFF.2p3; // scaled by exponent
+	double value2 = 0x1E.4p7; // scaled by exponent
+	std::cout << value1 << " + " << value2 << " = " << (value1+value2) << "\n";
+	double value3 = 0x1.2p3;
+	std::cout << value3 << " + " << value3 << " = " << (value3+value3) << "\n";
+	return 0;
+}
