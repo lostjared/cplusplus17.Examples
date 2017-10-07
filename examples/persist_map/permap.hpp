c889d4ad808b072e3b59eb46e654da70d5cbf2db
lostjared <jaredbruni@gmail.com>
2017-10-07 16:16:54 -0700

    added program

---
+++ examples/persist_map/permap.hpp
@@ -0,0 +1,124 @@
+#ifndef __PERMAP__H__
+#define __PERMAP__H__
+
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<fstream>
+
+template<typename T>
+void GenericRead(std::fstream &file, T &type) {
+    file.read(&type, sizeof(type));
+}
+template<typename T>
+void GenericWrite(std::fstream &file, const T &type) {
+    file.write(&type, sizeof(type));
+}
+
+template<typename T>
+class PersistMap {
+public:
+    PersistMap() = delete;
+    PersistMap(const PersistMap<T> &p);
+    PersistMap(PersistMap<T> &&p);
+    ~PersistMap();
+    typedef void (*Read)(std::fstream &file, T &type);
+    typedef void (*Write)(std::fstream &file, const T &type);
+    PersistMap(std::string filename, Read re, Write wr);
+    
+    void WriteMap();
+    void ReadMap();
+    T &operator[](const std::string &pos);
+    void set(std::string key, const T &type);
+    T &get(std::string key);
+    std::unordered_map<std::string, T> *operator->();
+private:
+    std::unordered_map<std::string, T> map_;
+    Read read_;
+    Write write_;
+    std::string filename;
+};
+
+
+template<typename T>
+PersistMap<T>::PersistMap(const PersistMap<T> &p) : map_(p.map_), read_(p.read_), write_(p.write_), filename(p.filename) {
+    ReadMap();
+}
+
+template<typename T>
+PersistMap<T>::PersistMap(PersistMap<T> &&p) : map_(std::move(p.map_)), read_(p.read_), write_(p.write), filename(p.filename) {
+    ReadMap();
+}
+
+template<typename T>
+PersistMap<T>::PersistMap(std::string filen, Read re, Write wr) : read_(re), write_(wr), filename(filen) {
+    ReadMap();
+}
+
+template<typename T>
+PersistMap<T>::~PersistMap() {
+    WriteMap();
+}
+
+template<typename T>
+T &PersistMap<T>::operator[](const std::string &pos) {
+    return map_[pos];
+}
+
+template<typename T>
+void PersistMap<T>::WriteMap() {
+    std::cout << "Writing to disk...\n";
+    std::fstream file;
+    file.open(filename, std::ios::out | std::ios::binary);
+    if(!file.is_open()) {
+        std::cerr << "Error file: " << filename << " could not be opened.\n";
+        return;
+    }
+    for(auto i = map_.begin(); i != map_.end(); ++i) {
+        std::string val = i->first;
+        unsigned int value = val.length();
+        file.write(reinterpret_cast<char*>(&value), sizeof(value));
+        file.write(val.c_str(), value);
+        write_(file, i->second);
+    }
+    file.close();
+}
+template<typename T>
+void PersistMap<T>::ReadMap() {
+    std::cout << "Reading from disk...\n";
+    std::fstream file;
+    file.open(filename, std::ios::in | std::ios::binary);
+    if(!file.is_open()) {
+        return;
+    }
+    while(!file.eof()) {
+        std::string key;
+        unsigned int value = 0;
+        file.read(reinterpret_cast<char*>(&value), sizeof(value));
+        char *buf = new char[value+1];
+        file.read(buf, value);
+        buf[value] = 0;
+        key = buf;
+        delete [] buf;
+        T type;
+        read_(file,type);
+        map_[key] = type;
+    }
+    file.close();
+}
+
+template<typename T>
+std::unordered_map<std::string, T> *PersistMap<T>::operator->() {
+    return &map_;
+}
+
+template<typename T>
+void PersistMap<T>::set(std::string key, const T &type) {
+    map_[key] = type;
+}
+template<typename T>
+T &PersistMap<T>::get(std::string key) {
+    return map_[key];
+}
+
+#endif
---
+++ examples/persist_map/persist_map.cpp
@@ -0,0 +1,33 @@
+#include "permap.hpp"
+#include<algorithm>
+
+int main() {
+    PersistMap<std::string> string_data("data.txt", [](std::fstream &file, std::string &val) {
+        unsigned int value;
+        file.read(reinterpret_cast<char*>(&value), sizeof(value));
+        char *buf = new char[value+1];
+        file.read(buf, value);
+        buf[value] = 0;
+        val = buf;
+        delete [] buf;
+    },
+    [](std::fstream &file, const std::string &val) {
+        unsigned int value = val.length();
+        file.write(reinterpret_cast<char*>(&value), sizeof(value));
+        file.write(val.c_str(), value);
+    });
+    while(1) {
+    	std::cout << "Enter key: ";
+    	std::string key_text;
+    	std::getline(std::cin, key_text);
+        if(key_text == "quit") break;
+    	std::cout << "Enter key value: ";
+    	std::string key_value;
+    	std::getline(std::cin, key_value);
+        string_data[key_text] = key_value;
+        for (auto i = string_data->begin(); i != string_data->end(); ++i) {
+            std::cout << i->first << " = " << i->second << "\n";
+        }
+    }
+    return 0;
+}
