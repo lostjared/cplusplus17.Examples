ce3868d19e03ba1f4e7244f00b290cbd05e409ad
lostjared <jaredbruni@gmail.com>
2017-11-04 10:46:08 -0700

    changed std::string to T

--- features/policy_and_traits/add_values.cpp
+++ features/policy_and_traits/add_values.cpp
@@ -95,7 +95,7 @@ public:
 template<typename T>
 class ValueStreamPolicy {
 public:
-    static std::string operation(std::string start, std::string val) {
+    static auto operation(T start, T val) {
         std::cout << "Stream output: " << val << "\n";
         return start;
     }
