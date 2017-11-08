dc400b2ac8758f052280e4370a8f552beb5006de
lostjared <jaredbruni@gmail.com>
2017-11-07 17:38:42 -0800

    added test for default constructor

--- features/inheritance/temp_inherit.cpp
+++ features/inheritance/temp_inherit.cpp
@@ -5,10 +5,23 @@
 #include<iostream>
 #include<string>
 
+// will be able to tell us whether or not a type has a default constructor
+template<typename T>
+class HasDefaultConstructor {
+private:
+    template<typename T1, typename = decltype(T1())>
+    static std::true_type test(void*);
+    
+    template<typename>
+    static std::false_type test(...);
+public:
+    static constexpr bool value = std::is_same<decltype(test<T>(nullptr)), std::true_type>::value;
+};
 
 template<typename M, typename B>
 class Index : public B {
 public:
+    static_assert(HasDefaultConstructor<M>::value == true, "No default constructor");
     Index() {
         //member = M();
         ++member;
@@ -38,9 +51,17 @@ public:
     }
 };
 
+class Test {
+public:
+    Test() = delete;
+    Test(int x) {}
+};
+
 using MemberCount = Index<int, Object>;
 
 int main() {
+    // Below will fail if uncommented.
+   //  Index<Test, Object> fails;
     MemberCount one;
     one.say("Hey ... ");
     for(unsigned int i = 0; i < 3; ++i) {
