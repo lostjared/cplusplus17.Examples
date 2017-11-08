e31739b489ca6f806b98228b277ca8cb92d70bb5
lostjared <jaredbruni@gmail.com>
2017-11-08 08:43:17 -0800

    + added new example

---
+++ features/compile_time/arr_average.cpp
@@ -0,0 +1,30 @@
+/*
+ Calculate Average of an Array at Compile Time
+*/
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<array>
+
+template<typename T, int size>
+constexpr std::size_t Average(const T *arr) {
+    T total = T();
+    for(unsigned int i = 0; i < size; ++i) {
+        total += arr[i];
+    }
+    return total/size;
+}
+
+template<typename T, std::size_t size, const T *arr>
+class AverageT {
+public:
+    static constexpr T value = Average<T, size>(arr);
+};
+
+constexpr int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int main() {
+    constexpr int v = AverageT<int, 10, arr>::value;
+    std::cout << "Average calculated at Runtime: " << v << "\n";
+    return 0;
+}
