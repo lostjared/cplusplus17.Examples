b061705a8bfc7d25f4c6d312529460f9bac7f446
lostjared <jaredbruni@gmail.com>
2017-11-11 00:37:31 -0800

    updated function-ch

--- features/function/function-ch.cpp
+++ features/function/function-ch.cpp
@@ -10,38 +10,35 @@ public:
     bool enabled;
 };
 
-std::vector<User> users { {"Jared", "Bruni", true}, {"Person", "Unknown", false}, {"Test1", "Test2",false}, {"Mr. ", "Robot", true}};
-
 // could do it like this
-void createListOfFirstNames(std::vector<std::string> &u) {
+void createListOfFirstNames(std::vector<User> &users, std::vector<std::string> &u) {
     for(auto i  = users.begin(); i != users.end(); ++i) {
         if(i->enabled) u.push_back(i->first);
     }
 }
-
-void createListOfLastName(std::vector<std::string> &u) {
+void createListOfLastName(std::vector<User> &users, std::vector<std::string> &u) {
     for(auto i  = users.begin(); i != users.end(); ++i) {
         if(i->enabled) u.push_back(i->first);
     }
 }
 
 // but less typing like this
+// pure function - only operates on types passed in as arguments
 template<typename F>
-void createList(std::vector<std::string> &u,F func) {
+void createList(bool enabled, std::vector<User> &users, std::vector<std::string> &u,F func) {
     for(auto i = users.begin(); i != users.end(); ++i) {
-        if(i->enabled == true) u.push_back(func(*i));
+        if(i->enabled == enabled) u.push_back(func(*i));
     }
 }
 
 int main() {
-    std::vector<std::string> first, last;
-    createList(first, [](User &u) -> std::string {
-        return u.first;
-    });
-    createList(last, [](User &u) -> std::string {
-        return u.last;
-    });
+    std::vector<User> users { {"Jared", "Bruni", true}, {"Person", "Unknown", false}, {"Test1", "Test2",false}, {"Mr. ", "Robot", true}};
+    std::vector<std::string> first, last,disabledfirst;
+    createList(true, users, first, [](User &u) -> std::string { return u.first; });
+    createList(true, users, last, [](User &u) -> std::string { return u.last; });
+    createList(false, users, disabledfirst, [](User &u) -> std::string { return u.first; });
     for(auto &i : first) std::cout << i << "\n";
     for(auto &i : last) std::cout << i << "\n";
+    for(auto &i : disabledfirst) std::cout << i << "\n";
     return 0;
 }
