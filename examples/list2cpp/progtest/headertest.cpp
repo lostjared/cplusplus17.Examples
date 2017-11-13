93971f4339de04f4acb650b41ef8453765a22ca6
lostjared <jaredbruni@gmail.com>
2017-11-12 16:41:26 -0800

    added program test

---
+++ examples/list2cpp/progtest/headertest.cpp
@@ -0,0 +1,12 @@
+#include"header.hpp"
+#include<iostream>
+
+int main() {
+    
+    
+    for(unsigned long i = 0; i < varname_size; ++i) {
+        std::cout << varname_arr[i] << "\n";
+    }
+    
+    return 0;
+}
