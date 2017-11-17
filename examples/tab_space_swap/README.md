176e3d058043671aec998b4232b319d80fabe38f
lostjared <jaredbruni@gmail.com>
2017-11-16 16:28:17 -0800

    Updated now can convert spaces into tabs

--- examples/tab_space_swap/README.md
+++ examples/tab_space_swap/README.md
@@ -1,10 +1,17 @@
 Simple program use:
 
-$ cat source.txt | tab2space.prog 4 >output.txt
+$ cat source.txt | tabs2space.prog t 4 >output.txt
 
 works with any output from other programs
 
-$ cat source.txt | tab2space.prog 7 
+$ cat source.txt | tabs2space.prog t 7 
+
+also use s to convert a number of spaces in a row to tab
+
+works with any output from other programs
+
+$ cat source.txt | tabs2space.prog s 4 >output.txt
+
 
 
 
--- examples/tab_space_swap/tabs2space.cpp
+++ examples/tab_space_swap/tabs2space.cpp
@@ -1,16 +1,28 @@
+
+/*
+
+- Jared
+
+
+*/
+
 #include<iostream>
 #include<string>
 #include<fstream>
 
 void swapTabSpace(std::istream &in, std::ostream &out, int tabs=4);
+void swapSpaceTab(std::istream &in, std::ostream &out, int spaces=4);
 
 int main(int argc, char **argv) {
-    if(argc != 2) {
+    if(argc != 3) {
         std::cerr << "must enter number of spaces as argument\n" << argv[0] << " num";
     }
-    int num = atoi(argv[1]);
+    int num = atoi(argv[2]);
     if(num > 0) {
-    	swapTabSpace(std::cin, std::cout, num);
+        if(argv[1][0] == 't')
+    		swapTabSpace(std::cin, std::cout, num);
+        else
+            swapSpaceTab(std::cin, std::cout, num);
     } else {
         std::cerr << "invalid number of spaces..\n";
     }
@@ -33,3 +45,29 @@ void swapTabSpace(std::istream &in, std::ostream &out, int tabs) {
         }
     }
 }
+
+void swapSpaceTab(std::istream &in, std::ostream &out, int spaces) {
+    while(!in.eof()) {
+        char c = in.get();
+        if(in) {
+            if(c == ' ') {
+                std::string buf;
+                buf += c;
+                do {
+                    c = in.get();
+                    if(in && c == ' ') {
+                        buf += c;
+                    }
+                } while(c == ' ');
+                if(buf.length() == spaces) {
+                    	out << "\t" << c;
+                } else {
+                    out << buf << c;
+                }
+            } else {
+                out << c;
+            }
+        }
+    }
+}
+
