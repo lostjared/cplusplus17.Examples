2723e16c60885b1b32fb9dfab0eabbce24a31270
lostjared <jaredbruni@gmail.com>
2017-11-19 09:05:48 -0800

    updated progtest outputs indivdiual values

--- examples/list2cpp/progtest/headertest.cpp
+++ examples/list2cpp/progtest/headertest.cpp
@@ -1,6 +1,7 @@
 #include"header.hpp"
 #include"outputbytes.hpp"
 #include<iostream>
+#include<iomanip>
 
 int main() {
     	for(unsigned long i = 0; i < varname_size; ++i) {
@@ -8,5 +9,9 @@ int main() {
     	}
 
 	std::cout << bytes_arr << "\n";
+
+	for(unsigned long i = 0; i < bytes_size; ++i) {
+		std::cout << std::dec << "[" << i << "] = " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<unsigned int>(bytes_arr[i]) << "\n";
+	}
 	return 0;
 }
