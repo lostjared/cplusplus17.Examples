cd3d6303ff42820b25dc21b45591dd1c4073cfd8
lostjared <jaredbruni@gmail.com>
2017-12-14 19:30:59 -0800

    added another tree

--- examples/tree/tree.cpp
+++ examples/tree/tree.cpp
@@ -36,7 +36,7 @@ public:
 private:
     void printNodes(Node<T> *n) {
         if(n != nullptr)
-            std::cout << "Node: " << n->token << ":" << n->id << "\n";
+            std::cout << "Node: " << n->token << ":" << static_cast<char>(n->id) << "\n";
         
         if(n->left != nullptr)
             printNodes(n->left);
@@ -58,7 +58,17 @@ private:
 
 int main() {
     Tree<std::string> data;
-    data.root->left = new Node<std::string>("Hello ", 1, new Node<std::string>(" World", 2, nullptr, nullptr), new Node<std::string>(" Tree!", 3, nullptr, nullptr));
+    data.root->left = new Node<std::string>("Hello ", 's', new Node<std::string>(" World", 's', nullptr, nullptr), new Node<std::string>(" Tree!", 's', nullptr, nullptr));
     data.print();
+    Tree<std::string> assign;
+    Node<std::string> *node;
+    assign.root->id = '=';
+    assign.root->left = new Node<std::string>("x", 'v', nullptr, nullptr);
+    assign.root->right = new Node<std::string>("10", 'n', nullptr, nullptr);
+    node = assign.root->right;
+    node->id = '+';
+    node->left = new Node<std::string>("x", 'v', nullptr, nullptr);
+    node->right = new Node<std::string>("y", 'v', nullptr, nullptr);
+    assign.print();
     return 0;
 }
