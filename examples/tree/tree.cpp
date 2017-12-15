812db843be81569a3732ab54969f9885a17100c3
lostjared <jaredbruni@gmail.com>
2017-12-15 10:38:00 -0800

    updated program moves through the tree and solves the expression

--- examples/tree/tree.cpp
+++ examples/tree/tree.cpp
@@ -1,16 +1,28 @@
 // Practice creating a tree.
+// Than moving through the tree and solving the expression
 
 #include<iostream>
 #include<string>
+#include<unordered_map>
+
+struct Symbol {
+    double value;
+    std::string name;
+};
+
+enum class Var_type { EMPTY, DIGIT, VARIABLE, PLUS, EQUAL};
+
+std::unordered_map<std::string, Symbol> symbols;
 
 template<typename T>
 class Node {
 public:
     T token;
-    unsigned int id;
+    double value;
+    Var_type id;
     Node<T> *left, *right;
-    Node() : id(0), left(nullptr), right(nullptr) {}
-    Node(const T &n_token, unsigned int n_id, Node<T> *n_left, Node<T> *n_right) {
+    Node() : id(Var_type::EMPTY), left(nullptr), right(nullptr) {}
+    Node(const T &n_token, Var_type n_id, Node<T> *n_left, Node<T> *n_right) {
         token = n_token;
         id = n_id;
         left = n_left;
@@ -24,7 +36,7 @@ public:
     Node<T> *root;
     
     Tree() {
-        root = new Node<T>("Root", 0, nullptr, nullptr);
+        root = new Node<T>("Root", Var_type::EMPTY, nullptr, nullptr);
     }
     ~Tree() {
         release(root);
@@ -33,6 +45,40 @@ public:
     void print() {
         printNodes(root);
     }
+    
+    double eval(Node<T> *node) {
+        if(node == nullptr) return 0;
+        std::cout << node->token << " current Node..\n";
+        double v = 0;
+    
+        switch(node->id) {
+            case Var_type::EQUAL: {
+                v = eval(node->right);
+                symbols[node->left->token].value = v;
+                std::cout << node->left->token << " = " << v << "\n";
+            }
+                break;
+            case Var_type::PLUS: {
+                double op1 = eval(node->left);
+                double op2 = eval(node->right);
+                std::cout << op1 << "+" << op2 << "\n";
+                return op1+op2;
+            }
+                break;
+            case Var_type::VARIABLE:
+                v = symbols[node->token].value;
+                std::cout << "Var Value: " << v << "\n";
+                break;
+            case Var_type::DIGIT:
+                v = node->value;
+                std::cout << "Value: " << v << "\n";
+                break;
+            case Var_type::EMPTY:
+                break;
+        }
+        return v;
+    }
+    
 private:
     void printNodes(Node<T> *n) {
         if(n != nullptr)
@@ -57,26 +103,34 @@ private:
     }
 };
 
+void add_token(std::string name, double value) {
+    symbols[name].name = name;
+    symbols[name].value = value;
+}
 
 int main() {
-    Tree<std::string> data;
-    data.root->left = new Node<std::string>("Hello ", 's', new Node<std::string>(" World", 's', nullptr, nullptr), new Node<std::string>(" Tree!", 's', nullptr, nullptr));
-    data.print();
-    
     // add some leaves to the tree
     Tree<std::string> assign;
     Node<std::string> *node;
-    assign.root->id = '=';
+    assign.root->id = Var_type::EQUAL;
     assign.root->token = "=";
-    assign.root->left = new Node<std::string>("x", 'v', nullptr, nullptr);
-    assign.root->right = new Node<std::string>("+", 'v', nullptr, nullptr);
+    assign.root->left = new Node<std::string>("x", Var_type::VARIABLE, nullptr, nullptr);
+    assign.root->right = new Node<std::string>("+", Var_type::PLUS, nullptr, nullptr);
+    Node<std::string> *plus = assign.root->right;
     node = assign.root->right;
-    node->left = new Node<std::string>("10", 'd', nullptr, nullptr);
-    node->right = new Node<std::string>("+", '+', nullptr, nullptr);
+    node->left = new Node<std::string>("10", Var_type::DIGIT, nullptr, nullptr);
+    node->left->value = 10;
+    node->right = new Node<std::string>("+", Var_type::PLUS, nullptr, nullptr);
     node = node->right;
-    node->left = new Node<std::string>("x", 'v', nullptr, nullptr);
-    node->right = new Node<std::string>("y", 'v', nullptr, nullptr);
+    node->left = new Node<std::string>("x", Var_type::VARIABLE, nullptr, nullptr);
+    node->right = new Node<std::string>("y", Var_type::VARIABLE, nullptr, nullptr);
     // print the leaves
     assign.print();
+    // put some variables in symbol table
+    add_token("x", 25);
+    add_token("y", 50);
+    // evaluate 
+    double val = assign.eval(assign.root);
+    std::cout << "value: " << val << "\n";
     return 0;
 }
