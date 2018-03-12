e92dd98699f84d545471323c239299732e2ab14b
lostjared <jaredbruni@gmail.com>
2018-03-12 11:09:35 -0700

    updated example

--- examples/stopwatch/main.cpp
+++ examples/stopwatch/main.cpp
@@ -3,15 +3,35 @@
 
 
 void testFunc() {
-    StopWatch<HighResolutionClock> start("testFunc");
     char a[2];
-    for(unsigned int i = 0; i < 100000; ++i)
-        a[0] = a[1] = 0;
+    for(unsigned int i = 0; i < 1920*1080; ++i)
+        a[0] = a[1] = rand()%255;
+}
+
+void averageTest() {
+    std::cout << "Testing: ";
+    long counter = 0;
+    for(int index = 0; index < 100; ++index) {
+        SystemClock clock;
+    	clock.start();
+    	testFunc();
+    	clock.stop();
+        counter += clock.timePassed();
+        std::cout << ".";
+        fflush(stdout);
+    }
+    std::cout << "\n";
+    counter /= 100;
+    std::cout << "Average time for testFunc: " << counter << " milliseconds\n";
+}
+
+void testFunc2() {
+    StopWatch<HighResolutionClock> counter("testFunc");
+    testFunc();
 }
 
 int main(int argc, char **argv) {
-    for(unsigned int i = 0; i < 100; ++i)
-        testFunc();
-    
+    averageTest();
+    testFunc2();
     return 0;
 }
