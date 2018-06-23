d7161be46042d1483b4db77c724881a6dfa29e97
lostjared <jaredbruni@gmail.com>
2018-06-23 06:58:55 -0700

    Finally got it this time

--- examples/quine/quine.cpp
+++ examples/quine/quine.cpp
@@ -10,19 +10,19 @@ int main(int argc, char **argv)
         "	const char *arr[] = {",
         " 	",
         "	};",
-        "	for(int i = 0; i < 6; ++i)",
-        "		std::cout << arr[i] << quote << std::endl;",
-        "	for(int i = 0; argv[i] != 0; ++i)",
-        "		std::cout << arr[6] << quote << arr[i] << quote << \",\";",
-        "	for(int i = 7; arr[i] != 0; ++i)",
+        "	for(int i = 0; i < 5; ++i)",
+        "		std::cout << arr[i] << std::endl;",
+        "	for(int i = 0; i < 15; ++i)",
+        "		std::cout << arr[5] << quote << arr[i] << quote << ',' << std::endl;",
+        "	for(int i = 5; i < 15; ++i)",
         "		std::cout << arr[i] << std::endl;",
         "	return 0;",
-        "}", 0};
+        "}"};
     for(int i = 0; i < 5; ++i)
         std::cout << arr[i] << std::endl;
-    for(int i = 0; arr[i] != 0; ++i)
+    for(int i = 0; i < 15; ++i)
         std::cout << arr[5] << quote << arr[i] << quote << "," << std::endl;
-    for(int i = 6; arr[i] != 0; ++i)
+    for(int i = 5; i < 15; ++i)
         std::cout << arr[i] << std::endl;
     return 0;
 }
