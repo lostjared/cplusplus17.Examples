5de6f618c2ef3dc0d98f6d10c8c45127fe609af3
lostjared <jaredbruni@gmail.com>
2018-06-22 19:16:55 -0700

    formatting

--- examples/quine/quine.cpp
+++ examples/quine/quine.cpp
@@ -2,26 +2,26 @@
 int main(int argc, char **argv) {
     char quote = 34;
     const char *arr[] = {
-	"#include<iostream>",
-    "int main(int argc, char **argv)",
-    "{",
-    "	char quote = 34;",
-    "	const char *arr[] = {",
-    " 	",
-    "	};",
-    "	for(int i = 0; i < 6; ++i)",
-    "		std::cout << arr[i] << quote << std::endl;",
-    "	for(int i = 0; argv[i] != 0; ++i)",
-    "		std::cout << arr[6] << quote << arr[i] << quote << \",\";",
-    "	for(int i = 7; arr[i] != 0; ++i)",
-    "		std::cout << arr[i] << std::endl;",
-    "	return 0;",
-    "}", 0};
-	for(int i = 0; i < 5; ++i)
-		std::cout << arr[i] << std::endl;
+        "#include<iostream>",
+        "int main(int argc, char **argv)",
+        "{",
+        "	char quote = 34;",
+        "	const char *arr[] = {",
+        " 	",
+        "	};",
+        "	for(int i = 0; i < 6; ++i)",
+        "		std::cout << arr[i] << quote << std::endl;",
+        "	for(int i = 0; argv[i] != 0; ++i)",
+        "		std::cout << arr[6] << quote << arr[i] << quote << \",\";",
+        "	for(int i = 7; arr[i] != 0; ++i)",
+        "		std::cout << arr[i] << std::endl;",
+        "	return 0;",
+        "}", 0};
+    for(int i = 0; i < 5; ++i)
+        std::cout << arr[i] << std::endl;
     for(int i = 4; arr[i] != 0; ++i)
         std::cout << arr[5] << quote << arr[i] << quote << "," << std::endl;
-	for(int i = 6; arr[i] != 0; ++i)
-		std::cout << arr[i] << std::endl;
-	return 0;
+    for(int i = 6; arr[i] != 0; ++i)
+        std::cout << arr[i] << std::endl;
+    return 0;
 }
