22fcf26143daa4180835aed2eb9bef294926491b
lostjared <jaredbruni@gmail.com>
2018-06-23 07:09:30 -0700

    small change

--- examples/quine/quine.cpp
+++ examples/quine/quine.cpp
@@ -1,28 +1,30 @@
 #include<iostream>
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    char quote = 34;
-    const char *arr[] = {
-        "#include<iostream>",
-        "int main(int argc, char **argv)",
-        "{",
-        "	char quote = 34;",
-        "	const char *arr[] = {",
-        " 	",
-        "	};",
-        "	for(int i = 0; i < 5; ++i)",
-        "		std::cout << arr[i] << std::endl;",
-        "	for(int i = 0; i < 15; ++i)",
-        "		std::cout << arr[5] << quote << arr[i] << quote << ',' << std::endl;",
-        "	for(int i = 5; i < 15; ++i)",
-        "		std::cout << arr[i] << std::endl;",
-        "	return 0;",
-        "}"};
-    for(int i = 0; i < 5; ++i)
-        std::cout << arr[i] << std::endl;
-    for(int i = 0; i < 15; ++i)
-        std::cout << arr[5] << quote << arr[i] << quote << "," << std::endl;
-    for(int i = 5; i < 15; ++i)
-        std::cout << arr[i] << std::endl;
-    return 0;
+	char quote = 34;
+	const char *arr[] = {
+ 	"#include<iostream>",
+ 	"int main(int argc, char **argv)",
+ 	"{",
+ 	"	char quote = 34;",
+ 	"	const char *arr[] = {",
+ 	" 	",
+ 	"	};",
+ 	"	for(int i = 0; i < 5; ++i)",
+ 	"		std::cout << arr[i] << std::endl;",
+ 	"	for(int i = 0; i < 15; ++i)",
+ 	"		std::cout << arr[5] << quote << arr[i] << quote << ',' << std::endl;",
+ 	"	for(int i = 5; i < 15; ++i)",
+ 	"		std::cout << arr[i] << std::endl;",
+ 	"	return 0;",
+ 	"}",
+ 	
+	};
+	for(int i = 0; i < 5; ++i)
+		std::cout << arr[i] << std::endl;
+	for(int i = 0; i < 15; ++i)
+		std::cout << arr[5] << quote << arr[i] << quote << ',' << std::endl;
+	for(int i = 5; i < 15; ++i)
+		std::cout << arr[i] << std::endl;
+	return 0;
 }
