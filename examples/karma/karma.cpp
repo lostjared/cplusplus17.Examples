1480ceed5c28c0822248611a0720d3aee6548091
lostjared <jaredbruni@gmail.com>
2018-09-25 15:06:03 -0700

    poem written in C++ about life

---
+++ examples/karma/karma.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+class Thought {
+public:
+    std::string idea;
+};
+
+Thought &get(Thought &thought) {
+    std::cout << "Got info: " << thought.idea << "\n";
+    return thought;
+}
+Thought &give(Thought &thought) {
+    std::cout << "Gave thought " << thought.idea << "\n";
+    return thought;
+}
+
+void life() {
+    Thought code;
+    code.idea = "Cycle\n";
+    Thought think = get(give(code));
+    std::cout << "Karma returns: " << think.idea << "\n";
+}
+
+
+int main(int argc, char **argv) {
+    while(1) {
+    	life();
+    }
+	return 0;
+}
