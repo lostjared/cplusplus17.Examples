fb0303ec20eeda9d4bdcf9a71a5f721efc6469c6
Jared Bruni <jaredbruni@gmail.com>
2021-02-28 11:33:09 -0800

    karma trail

---
+++ examples/karma_trail/karma-trail.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+class Escape {};
+
+std::string give() {
+    std::string data;
+    std::cout << "Enter: ";
+    std::getline(std::cin, data);
+    return data;
+}
+
+std::string get(std::string data) {
+    if(data == "love")
+        std::cout << "love sent...\n";
+    else if(data == "fear")
+        std::cout << "fear sent...\n";
+    else if(data == "break")
+        throw Escape();
+    
+    return data;
+}
+
+int main() {
+    try {
+        std::string trail;
+        while(1) {
+            trail += get(give()) + "\n";
+            std::cout << "karma trail: " << trail << "\n";
+        }
+    }
+    catch(Escape &e) {
+        std::cout << "freedom..\n";
+    }
+    return 0;
+}
