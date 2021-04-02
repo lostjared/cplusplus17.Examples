e4f7f29bd38ce830acd97f4a9aabf53bdd13948a
Jared Bruni <jaredbruni@gmail.com>
2021-04-02 02:07:22 -0700

    updated

---
+++ examples/cmd-switch/source/print-lines-test.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<algorithm>
+#include"cmd-switch.hpp"
+
+int main(int argc, char **argv) {
+    try {
+        cmd::ArgumentList<std::string> argz(argc, argv);
+        std::string filename;
+        argz.require("--file", filename);
+        bool line_sep=argz.check("--sep");
+        std::fstream file;
+        file.open(filename, std::ios::in);
+        if(!file.is_open()) {
+            std::cerr << "Error could not open file: "<< argv[1] << "\n";
+            exit(EXIT_FAILURE);
+        }
+        unsigned int index = 1;
+        std::string sep = ": ";
+        if(line_sep == false)
+            sep = " ";
+        
+        while(!file.eof()) {
+            std::string line;
+            std::getline(file, line);
+            if(file)
+                std::cout << index++ << sep << line << "\n";
+        }
+        file.close();
+        exit(EXIT_SUCCESS);
+    }
+    catch(cmd::ArgExcep<std::string> &e) {
+        std::cerr << e.what() << "\n";
+    }
+    return 0;
+}
