68699be511ae5e47470a457fca2710daf08e9824
Jared Bruni <jaredbruni@gmail.com>
2021-04-04 00:36:49 -0700

    updated

--- examples/libacidcam/main.cpp
+++ examples/libacidcam/main.cpp
@@ -1,27 +1,34 @@
 #include<iostream>
 #include"ac.h"
+#include"cmd-switch.hpp"
 
 int main(int argc, char **argv) {
-    ac::init();
-    if(argc != 3) {
-        std::cerr << "Requires two argument..\nVideo file/filter..\n";
-        exit(EXIT_FAILURE);
-    }
-    cv::VideoCapture cap(argv[1]);
-    if(!cap.isOpened()) {
-        std::cerr << "Could not open vidoe file: "<< argv[1] << "\n";
-    }
-    
-    bool active = true;
-    cv::namedWindow("acidcam");
-    while(active) {
-        cv::Mat frame;
-        if(cap.read(frame)) {
-            ac::CallFilter(atoi(argv[2]), frame);
-            cv::imshow("acidcam", frame);
-            int key = cv::waitKey(10);
-            if(key == 27) exit(EXIT_SUCCESS);
-        } else break;
+    try {
+        cmd::ArgumentList<std::string> argz(argc, argv);
+        std::string filename;
+        std::string filter;
+        argz.require("--file", filename, "input video file");
+        argz.require("--filter", filter, "filter index");
+        
+        ac::init();
+        cv::VideoCapture cap(filename);
+        if(!cap.isOpened()) {
+            std::cerr << "Could not open vidoe file: "<< filename << "\n";
+        }
+        
+        bool active = true;
+        cv::namedWindow("acidcam");
+        while(active) {
+            cv::Mat frame;
+            if(cap.read(frame)) {
+                ac::CallFilter(atoi(filter.c_str()), frame);
+                cv::imshow("acidcam", frame);
+                int key = cv::waitKey(10);
+                if(key == 27) exit(EXIT_SUCCESS);
+            } else break;
+        }
+    } catch(cmd::ArgExcep<std::string> &e) {
+        std::cerr << e.what() << "\n";
     }
     return 0;
 }
