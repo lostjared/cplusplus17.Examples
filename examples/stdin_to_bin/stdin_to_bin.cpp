52b223234b11634db1aa5544eb84dcc4e2422c54
Jared Bruni <jaredbruni@gmail.com>
2021-04-07 03:27:08 -0700

    added stdin_to_bin

---
+++ examples/stdin_to_bin/stdin_to_bin.cpp
@@ -0,0 +1,19 @@
+#include"cmd-switch.hpp"
+#include<iostream>
+
+
+int main(int argc, char **argv) {
+    std::cout << "unsigned char array[] = {\n";
+    int count = 0;
+    while(!std::cin.eof()) {
+        char c = std::cin.get();
+        if(std::cin) {
+            std::cout << "0x" << std::hex << static_cast<unsigned int>(c) << ",";
+            ++count;
+        }
+    }
+    std::cout << "\n0 };\n";
+    std::cout << "unsigned int array_count = " << count << ";\n";
+    
+    return 0;
+}
