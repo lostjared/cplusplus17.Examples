f518d04bf98e5851d5988b36b04e85319e35e564
Jared Bruni <jaredbruni@gmail.com>
2021-04-07 03:43:41 -0700

    updated

--- examples/stdin_to_bin/stdin_to_bin.cpp
+++ examples/stdin_to_bin/stdin_to_bin.cpp
@@ -12,14 +12,14 @@ int main(int argc, char **argv) {
     std::cout << "unsigned char array[] = {\n";
     int count = 0;
     while(!std::cin.eof()) {
-        char c = std::cin.get();
+        unsigned char c = std::cin.get();
         if(std::cin) {
-            std::cout << "0x" << std::hex << static_cast<unsigned int>(c) << ",";
+            std::cout << "0x" << std::hex << (unsigned int)(unsigned char)c << ",";
             ++count;
         }
     }
     std::cout << "\n0 };\n";
-    std::cout << "unsigned int array_count = " << count << ";\n";
+    std::cout << "unsigned int array_count = 0x" << std::hex << count << ";\n";
     
     return 0;
 }
