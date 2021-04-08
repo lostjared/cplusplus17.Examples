bbe40daf75eeb2dd8cbaa8985e64be7ef898cb5f
Jared Bruni <jaredbruni@gmail.com>
2021-04-08 02:14:05 -0700

    added cpp shift

--- examples/stdin_to_bin/stdin_to_bin.cpp
+++ examples/stdin_to_bin/stdin_to_bin.cpp
@@ -9,17 +9,24 @@
 
 
 int main(int argc, char **argv) {
-    std::cout << "unsigned char array[] = {\n";
-    unsigned long count = 0;
-    while(!std::cin.eof()) {
-        unsigned char c = std::cin.get();
-        if(std::cin) {
-            std::cout << "0x" << std::hex << static_cast<unsigned int>(static_cast<unsigned char>(c)) << ",";
-            ++count;
-        }
-    }
-    std::cout << "\n0 };\n";
-    std::cout << "unsigned long array_count = 0x" << std::hex << count << ";\n";
     
+    try {
+        
+        std::cout << "unsigned char array[] = {\n";
+        unsigned long count = 0;
+        while(!std::cin.eof()) {
+            unsigned char c = std::cin.get();
+            if(std::cin) {
+                std::cout << "0x" << std::hex << static_cast<unsigned int>(static_cast<unsigned char>(c)) << ",";
+                ++count;
+            }
+        }
+        std::cout << "\n0 };\n";
+        std::cout << "unsigned long array_count = 0x" << std::hex << count << ";\n";
+        
+    }
+    catch(cmd::ArgExcep<std::sttring> &e) {
+        std::cerr << e.what() << "\N":
+    }
     return 0;
 }
