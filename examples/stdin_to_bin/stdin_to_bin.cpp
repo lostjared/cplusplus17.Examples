05e74fa3daf20373c8bfe26cf90d750b4da83939
Jared Bruni <jaredbruni@gmail.com>
2021-04-09 02:02:12 -0700

    updated

--- examples/stdin_to_bin/stdin_to_bin.cpp
+++ examples/stdin_to_bin/stdin_to_bin.cpp
@@ -7,6 +7,7 @@
 // Argument Types
 // --name [variable name ]
 // --type [ variable type ]
+// --lang [ python/cpp ]
 
 #include"cmd-switch.hpp"
 #include<iostream>
@@ -23,23 +24,49 @@ int main(int argc, char **argv) {
         std::string type;
         bool type_;
         type_=argz.extract("--type", type);
-        if(!type_)
-            type = "unsigned char";
         
-        std::cout << type << " " << cmd_name << " [] = {\n";
-        unsigned long count = 0;
-        while(!std::cin.eof()) {
-            unsigned char c = 0;
-            std::cin.read(reinterpret_cast<char*>(&c),sizeof(c));
-            
-            if(std::cin) {
-                std::cout << "0x" << std::hex << static_cast<unsigned int>(static_cast<unsigned char>(c)) << ",";
-                ++count;
-            }
+        bool language_;
+        std::string lang;
+        int lang_type = 0;
+        language_=argz.extract("--lang",lang);
+        if(!language_) {
+            lang_type = 0;
         }
-        std::cout << "\n0 };\n";
-        std::cout << "unsigned long " << cmd_name << "_count = 0x" << std::hex << count << ";\n";
+        if(lang == "python")
+            lang_type = 1;
+        else
+            lang_type = 0;
         
+            if(lang_type == 0) {
+            if(!type_)
+                type = "unsigned char";
+            
+            std::cout << type << " " << cmd_name << " [] = {\n";
+            unsigned long count = 0;
+            while(!std::cin.eof()) {
+                unsigned char c = 0;
+                std::cin.read(reinterpret_cast<char*>(&c),sizeof(c));
+                
+                if(std::cin) {
+                    std::cout << "0x" << std::hex << static_cast<unsigned int>(static_cast<unsigned char>(c)) << ",";
+                    ++count;
+                }
+            }
+            std::cout << "\n0 };\n";
+            std::cout << "unsigned long " << cmd_name << "_count = 0x" << std::hex << count << ";\n";
+        }
+        else {
+            std::cout << cmd_name << " = [";
+            while(!std::cin.eof()) {
+                unsigned char c = 0;
+                std::cin.read(reinterpret_cast<char*>(&c),sizeof(c));
+                
+                if(std::cin) {
+                    std::cout <<  static_cast<unsigned int>(c) << ",";
+                }
+            }
+            std::cout << "0 ]\n";
+        }
     }
     catch(cmd::ArgExcep<std::string> &e) {
         std::cerr << e.what() << "\n";
