8997e132084cccd0f9f953ec2c1a7adb0f09ff6f
Jared Bruni <jaredbruni@gmail.com>
2021-04-25 15:26:00 -0700

    comments

--- examples/shell_cmd/cmd.cpp
+++ examples/shell_cmd/cmd.cpp
@@ -7,6 +7,10 @@
  ex:
  ./shell-cmd . "cat %f" .txt
  
+ actual example extract all archives:
+ 
+ $ shell-cmd . "7z e %f" .7z
+ 
  to execute command for each file type within directory/subdirectory
  just written to practice
   
