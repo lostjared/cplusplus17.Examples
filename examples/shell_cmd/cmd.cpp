83b09f99d5170254f51df391776246cf9e503c9e
Jared Bruni <jaredbruni@gmail.com>
2021-04-27 02:34:08 -0700

    better system

--- examples/shell_cmd/cmd.cpp
+++ examples/shell_cmd/cmd.cpp
@@ -23,6 +23,8 @@
 #include<dirent.h>
 #include<sys/types.h>
 #include<sys/stat.h>
+#include<sys/wait.h>
+#include<signal.h>
 #include<sstream>
 #include<cstdlib>
 #include<regex>
@@ -73,6 +75,57 @@ void add_directory(std::string path, std::vector<std::string> &files, std::strin
     closedir(dir);
 }
 
+int System(const std::string &command) {
+    sigset_t bmask, omask;
+    struct sigaction sa_ignore, sa_oquit, sa_origint, sa_default;
+    pid_t id;
+    int status, serrno;
+    
+    if(command == "") return System(":") == 0;
+    
+    sigemptyset(&bmask);
+    sigaddset(&bmask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &bmask, &omask);
+    sa_ignore.sa_handler = SIG_IGN;
+    sa_ignore.sa_flags = 0;
+    sigemptyset(&sa_ignore.sa_mask);
+    sigaction(SIGINT, &sa_ignore, &sa_origint);
+    sigaction(SIGQUIT, &sa_ignore, &sa_oquit);
+    
+    switch((id = fork())) {
+        case -1:
+            status = -1;
+            break;
+        case 0:
+            sa_default.sa_handler = SIG_DFL;
+            sa_default.sa_flags = 0;
+            sigemptyset(&sa_default.sa_mask);
+            if(sa_origint.sa_handler != SIG_IGN)
+                sigaction(SIGINT, &sa_default, NULL);
+            if(sa_oquit.sa_handler != SIG_IGN)
+                sigaction(SIGQUIT, &sa_default, NULL);
+            
+            execl("/bin/sh", "sh", "-c", command.c_str(), (char*)NULL);
+            _exit(127);
+            break;
+        default:
+            while(waitpid(id, &status, 0) == -1) {
+                if(errno != EINTR) {
+                    status = -1;
+                    break;
+                }
+            }
+            break;
+    }
+    serrno = errno;
+    sigprocmask(SIG_SETMASK, &omask, NULL);
+    sigaction(SIGINT, &sa_origint, NULL);
+    sigaction(SIGQUIT, &sa_oquit, NULL);
+    errno = serrno;
+    return status;
+}
+
+
 int main(int argc, char **argv) {
     if(argc != 4) {
         std::cerr << "Error: requires three arguments..\n";
@@ -91,7 +144,7 @@ int main(int argc, char **argv) {
         std::string cmd = argv[2];
         std::string r = replace_string(cmd, "%f", text);
         std::cout << r << "\n";
-        system(r.c_str());
+        System(r.c_str());
     }
     return 0;
 }
