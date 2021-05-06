31233e72762f06f3bc2a2a7ef5e52fdce65efb30
Jared Bruni <jaredbruni@gmail.com>
2021-05-06 03:14:39 -0700

    update

--- examples/regex/r.cpp
+++ examples/regex/r.cpp
@@ -4,7 +4,18 @@
 
 int main(int argc, char **argv) {
     
-    
+    if(argc != 2) {
+        std::cerr << "Error requires one argument..\n";
+        exit(EXIT_FAILURE);
+    }
+    while(!std::cin.eof()) {
+        std::string s;
+        std::getline(std::cin, s);
+        std::regex r(argv[1]);
+        if(std::cin && regex_search(s, r)) {
+            std::cout << argv[0] << ": match!\n";
+        }
+    }
     
     return 0;
 }
