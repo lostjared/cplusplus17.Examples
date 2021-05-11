67fd43e48cf571b5329e088a6636d465e1af933d
Jared Bruni <jaredbruni@gmail.com>
2021-05-10 17:16:00 -0700

    update

--- examples/hash/hash.cpp
+++ examples/hash/hash.cpp
@@ -6,85 +6,8 @@
 #include<string>
 #include<list>
 
-namespace htable {
-    // from Compilers PT&T
-    unsigned int hash(const std::string &key, unsigned int size) {
-        unsigned int val = 0;
-        const char *ptr = key.c_str();
-        while(*ptr != '\0') {
-            unsigned int tmp;
-            val = (val << 4) + (*ptr);
-            if((tmp = (val & 0xf00000000))) {
-                val = val ^ (tmp >> 24);
-                val = val ^ tmp;
-            }
-            ptr++;
-        }
-        return val%size;
-    }
+#include "hash.hpp"
 
-    template<typename T, unsigned int Size>
-    class Table {
-      
-    public:
-        Table() = default;
-
-        T &operator[](const std::string &key) {
-            int k = hash(key, Size);
-            for(auto it = buckets[k].begin(); it != buckets[k].end(); it++) {
-                if(it->first == key)
-                    return it->second;
-            }
-            buckets[k].push_back(std::make_pair(key, T()));
-            auto it = buckets[k].begin();
-            for(; it != buckets[k].end(); it++) {
-                if(it->first == key)
-                    break;
-            }
-            return it->second;
-        }
-        
-        void enter(std::string key, const T &type) {
-            unsigned int k = hash(key, Size);
-            for(auto it = buckets[k].begin(); it != buckets[k].end(); ++it) {
-                if(it->first == key) {
-                    it->second = type;
-                    return;
-                }
-            }
-            buckets[k].push_back(std::make_pair(key, type));
-        }
-        
-        bool lookup(std::string key, T &type) {
-            int k = hash(key, Size);
-            for(auto it = buckets[k].begin(); it != buckets[k].end(); ++it) {
-                if(it->first == key) {
-                    type = it->second;
-                    return true;
-                }
-            }
-            return false;
-        }
-        
-        void erase(std::string key) {
-            int k = hash(key, Size);
-            for(auto it = buckets[k].begin(); it != buckets[k].end(); ++it) {
-                if(it->first == key) {
-                    buckets[k].erase(it);
-                    return;
-                }
-            }
-        }
-        
-        const int size() const { return Size; }
-        std::list<std::pair<std::string, T>> &at(unsigned int pos) { return buckets[pos]; }
-        
-    protected:
-        std::list<std::pair<std::string, T>> buckets[Size];
-        
-    };
-
-}
 
 int main(int argc, char **argv) {
     htable::Table<std::string, 1024> table;
