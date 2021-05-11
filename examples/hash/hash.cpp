6078bfe2c396535c0806bb2ed71c3686dd6684cf
Jared Bruni <jaredbruni@gmail.com>
2021-05-10 17:00:17 -0700

    update

--- examples/hash/hash.cpp
+++ examples/hash/hash.cpp
@@ -88,24 +88,21 @@ namespace htable {
 
 int main(int argc, char **argv) {
     htable::Table<std::string, 1024> table;
-    table.enter("apple", "tree");
-    table.enter("bear", "animal");
-    std::string apple;
-    if(table.lookup("apple", apple)) {
-        std::cout << apple << "\n";
-    }
-    table.erase("apple");
-    if(table.lookup("apple", apple)) {
-        std::cout << apple << "\n";
-    }
-    table["order"] = "pie";
-    std::cout << table["order"] << "\n";
-    // print table
-    std::cout << "*****\tprint table\n";
-    for(unsigned int i = 0; i < table.size(); ++i) {
-        if(table.at(i).size() > 0) {
-            for(auto it = table.at(i).begin(); it != table.at(i).end(); ++it) {
-                std::cout << it->first << ":" << it->second << "\n";
+    while(1) {
+        std::string key;
+        std::string value;
+        std::cout << "enter key: ";
+        std::getline(std::cin, key);
+        std::cout << "enter value: ";
+        std::getline(std::cin, value);
+        table[key] = value;
+        // print table
+        std::cout << "*****\tprint table\n";
+        for(unsigned int i = 0; i < table.size(); ++i) {
+            if(table.at(i).size() > 0) {
+                for(auto it = table.at(i).begin(); it != table.at(i).end(); ++it) {
+                    std::cout << it->first << ":" << it->second << "\n";
+                }
             }
         }
     }
