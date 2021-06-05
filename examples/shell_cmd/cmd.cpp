1c1b33209ec92593c5124cccd56feb3c181dda0c
Jared Bruni <jaredbruni@gmail.com>
2021-06-05 12:18:17 -0700

    correct search pattern

--- examples/shell_cmd/cmd.cpp
+++ examples/shell_cmd/cmd.cpp
@@ -5,11 +5,11 @@
  
  ./shell-cmd path "command %f" regex_search_pattern
  ex:
- ./shell-cmd . "cat %f" .txt
+ ./shell-cmd . "cat %f" *.txt
  
  actual example extract all archives:
  
- $ shell-cmd . "7z e %f" .7z
+ $ shell-cmd . "7z e %f" *.7z
  
  to execute command for each file type within directory/subdirectory
  just written to practice
