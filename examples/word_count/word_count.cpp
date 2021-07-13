70e59333b4016ecc47ed58c863c979ac488aba2d
Jared Bruni <jaredbruni@gmail.com>
2021-07-13 10:19:22 -0700

    updated

--- examples/word_count/word_count.cpp
+++ examples/word_count/word_count.cpp
@@ -8,15 +8,21 @@
 #include<vector>
 #include<algorithm>
 
-void sort_and_print(std::unordered_map<std::string, unsigned int> &words);
+void extract_and_fill(std::istream &in, std::unordered_map<std::string, unsigned int> &words);
+void sort_and_print(std::ostream &out, std::unordered_map<std::string, unsigned int> &words);
 
 int main(int argc, char **argv) {
+    std::unordered_map<std::string, unsigned int> words;
+    extract_and_fill(std::cin, words);
+    sort_and_print(std::cout, words);
+    return 0;
+}
+
+void extract_and_fill(std::istream &in, std::unordered_map<std::string, unsigned int> &words) {
     std::string token;
     bool on = true;
-    std::unordered_map<std::string, unsigned int> words;
-    while(!std::cin.eof()) {
-        char c = std::cin.get();
-        c = tolower(c);
+    while(!in.eof()) {
+        char c = tolower(in.get());
         if(c >= 'a' && c <= 'z') {
             if(on == true) {
                 token += c;
@@ -33,17 +39,15 @@ int main(int argc, char **argv) {
     if(token != "") {
         words[token]++;
     }
-    sort_and_print(words);
-    return 0;
 }
 
-void sort_and_print(std::unordered_map<std::string, unsigned int> &words) {
+void sort_and_print(std::ostream &out, std::unordered_map<std::string, unsigned int> &words) {
     std::vector<std::string> keys;
     for(auto &i : words) {
         keys.push_back(i.first);
     }
     std::sort(keys.begin(), keys.end());
     for(auto &i : keys) {
-        std::cout << i << "  " << words[i] << "\n";
+        out << i << "  " << words[i] << "\n";
     }
 }
