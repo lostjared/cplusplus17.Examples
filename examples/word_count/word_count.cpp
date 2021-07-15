facbe264793dce257219d0ac82268052f6fe30b7
Jared Bruni <jaredbruni@gmail.com>
2021-07-14 19:17:48 -0700

    update

--- examples/word_count/word_count.cpp
+++ examples/word_count/word_count.cpp
@@ -1,23 +1,18 @@
 // $ cat text_file | ./word_count
-
 #include<iostream>
 #include<string>
 #include<cctype>
-#include<cstdlib>
 #include<unordered_map>
 #include<vector>
 #include<algorithm>
-
 void extract_and_fill(std::istream &in, std::unordered_map<std::string, unsigned int> &words);
 void sort_and_print(std::ostream &out, std::unordered_map<std::string, unsigned int> &words);
-
 int main(int argc, char **argv) {
     std::unordered_map<std::string, unsigned int> words;
     extract_and_fill(std::cin, words);
     sort_and_print(std::cout, words);
     return 0;
 }
-
 void extract_and_fill(std::istream &in, std::unordered_map<std::string, unsigned int> &words) {
     std::string token;
     bool on = true;
@@ -38,7 +33,6 @@ void extract_and_fill(std::istream &in, std::unordered_map<std::string, unsigned
     if(token != "")
         words[token]++;
 }
-
 void sort_and_print(std::ostream &out, std::unordered_map<std::string, unsigned int> &words) {
     std::vector<std::string> keys;
     for(auto &i : words)
