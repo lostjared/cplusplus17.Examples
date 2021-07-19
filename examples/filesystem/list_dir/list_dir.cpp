eee44bc1cc71ab8df282f724f22f895685ae1813
Jared Bruni <jaredbruni@gmail.com>
2021-07-19 13:04:07 -0700

    list dir

---
+++ examples/filesystem/list_dir/list_dir.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<filesystem>
+
+int main(int argc, char **argv) {
+    if(argc == 1) {
+        std::cerr << argv[0] << " directory \n";
+        std::cerr << argv[0] << " direcotyr -r (recursive)\n";
+        return 0;
+    }
+    if(argc == 3 && std::string(argv[2]) == "-r") {
+        for(auto &i : std::filesystem::recursive_directory_iterator(argv[1])) {
+            std::cout << i.path() << "\n";
+        }
+    }
+    if(argc == 2) {
+        for(auto &i : std::filesystem::directory_iterator(argv[1])) {
+            std::cout << i.path() << "\n";
+        }
+    }
+    return 0;
+}
