7f188b6c12ea9adeff4fe70de5ec5c1d1be573d9
Jared Bruni <jaredbruni@gmail.com>
2022-06-29 02:20:57 -0700

    updated

--- features/string/string-test.cpp
+++ features/string/string-test.cpp
@@ -1,13 +1,25 @@
-// convert integer to string
+
 #include<iostream>
 #include<string>
 
+int count_zeros(const std::string &text) {
+    int count = 0;
+    for(auto &i : text) {
+        if(i == '0') ++count;
+    }
+    return count;
+}
+
 int main() {
+    // convert integer to string
     std::cout << "Value: ";
     int x = 0;
     std::cin >> x;
     std::string temp;
     temp = std::to_string(x);
     std::cout << "value is: " + temp << "\n";
+    std::cout << "number of zeros: " << count_zeros(temp) << "\n";
+    std::cout << count_zeros("1000") << " zeros in 1000\n";
+    
     return 0;
 }
