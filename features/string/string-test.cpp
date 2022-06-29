a0e3680f5cbd32ee03373678cada13f3d14f3fd7
Jared Bruni <jaredbruni@gmail.com>
2022-06-29 02:25:26 -0700

    count_chars

--- features/string/string-test.cpp
+++ features/string/string-test.cpp
@@ -10,6 +10,15 @@ int count_zeros(const std::string &text) {
     return count;
 }
 
+template<typename Iter>
+int count_chars(char c, Iter start, Iter stop) {
+    int count = 0;
+    for(Iter i = start; i != stop; ++i) {
+        if(*i == c) ++count;
+    }
+    return count;
+}
+
 int main() {
     // convert integer to string
     std::cout << "Value: ";
@@ -18,6 +27,8 @@ int main() {
     std::string temp;
     temp = std::to_string(x);
     std::cout << "value is: " + temp << "\n";
+    std::cout << count_chars('0', temp.begin(), temp.end()) << " zeros in string with template.\n";
+    
     std::cout << "number of zeros: " << count_zeros(temp) << "\n";
     std::cout << count_zeros("1000") << " zeros in 1000\n";
     
