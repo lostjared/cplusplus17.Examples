94963dfb22cefd8aaaee90b407e8d1a04b222823
Jared Bruni <jaredbruni@gmail.com>
2022-06-30 09:25:01 -0700

    optional

---
+++ features/optional/op.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<optional>
+#include<string>
+
+std::optional<int> getCmd() {
+    std::optional<int> op;
+    std::cout << "enter command: (quit/print) ";
+    std::string s;
+    std::cin >> s;
+    if(s == "quit")
+        op = 1;
+    else if(s == "print")
+        op = 2;
+   
+    return op;
+}
+
+int main() {
+
+    auto i = getCmd();
+    if(i) {
+        std::cout << "Command is: " << *i << "\n";
+        switch(*i) {
+            case 1:
+                std::cout << "Goodbye!.\n";
+                exit(0);
+                break;
+            case 2:
+                std::cout << "Print...\n";
+                break;
+        }
+    }
+    
+    
+    
+    return 0;
+}
