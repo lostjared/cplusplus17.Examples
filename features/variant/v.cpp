6856f176ac8ee16d54173195f29419565b8ab9f2
Jared Bruni <jaredbruni@gmail.com>
2022-07-01 02:48:11 -0700

    variant ex

---
+++ features/variant/v.cpp
@@ -0,0 +1,16 @@
+#include<variant>
+#include<iostream>
+#include<string>
+#include<vector>
+
+using VType = std::variant<int, std::string>;
+
+int main() {
+    std::vector<VType> v{10, "Hello", 25};
+    for(auto &i : v) {
+        std::visit([](const auto &obj) {
+            std::cout << obj << "\n";
+        }, i);
+    }
+    return 0;
+}
