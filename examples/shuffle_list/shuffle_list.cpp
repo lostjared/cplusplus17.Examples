0a79b772f2a4ea04c80e796672863aaa9ffef5f2
Jared Bruni <jaredbruni@gmail.com>
2022-08-02 09:57:01 -0700

    shuffle list tool

--- examples/shuffle_list/shuffle_list.cpp
+++ examples/shuffle_list/shuffle_list.cpp
@@ -1,10 +1,24 @@
 #include<iostream>
 #include<fstream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<chrono>
+#include<random>
 
 
 int main(int argc, char **argv) {
-    
-    
-    
+    std::vector<std::string> vec;
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    while(!std::cin.eof()) {
+        std::string s;
+        std::getline(std::cin, s);
+        if(std::cin)
+            vec.push_back(s);
+    }
+    std::shuffle(vec.begin(), vec.end(), std::default_random_engine(seed));
+    for(auto &s : vec) {
+        std::cout << s << "\n";
+    }
     return 0;
 }
