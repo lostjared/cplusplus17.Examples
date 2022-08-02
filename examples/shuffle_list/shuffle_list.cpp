44427b4d705199350ef8bf4bce68cac9747c7ec3
Jared Bruni <jaredbruni@gmail.com>
2022-08-02 10:15:15 -0700

    Even better now reads file to

--- examples/shuffle_list/shuffle_list.cpp
+++ examples/shuffle_list/shuffle_list.cpp
@@ -7,14 +7,29 @@
 #include<random>
 
 
+void read_stream(std::istream &in, std::vector<std::string> &out) {
+    while(!in.eof()) {
+        std::string s;
+        std::getline(in, s);
+        if(in)
+            out.push_back(s);
+    }
+}
+
 int main(int argc, char **argv) {
     std::vector<std::string> vec;
     unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-    while(!std::cin.eof()) {
-        std::string s;
-        std::getline(std::cin, s);
-        if(std::cin)
-            vec.push_back(s);
+    if(argc == 1) {
+        read_stream(std::cin, vec);
+    } else if(argc == 2) {
+        std::fstream file;
+        file.open(argv[1], std::ios::in);
+        if(file.is_open())
+            read_stream(file, vec);
+        else {
+            std::cerr << "Error could not read file...\n";
+            exit(EXIT_FAILURE);
+        }
     }
     std::shuffle(vec.begin(), vec.end(), std::default_random_engine(seed));
     for(auto &s : vec) {
